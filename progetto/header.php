1a5da15f5de5fe03b0aa3909e68c7c39e6842d1d
Emanuele St <skiter@hotmail.it>
2025-01-22 19:09:39 +0100

    Add files via upload

---
+++ progetto/header.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Sito web con include</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Benvenuti nel mio sito</h1>
+        
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</li>
+                    <li><a href="contatti.php">Contatti</li>
+                </ul>    
+            </nav>
+        </header>
+   
